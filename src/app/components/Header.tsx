e9420df7eef8472eb6b87ad731d5b83fe2da1fa5
Max Knivets <knivets.m@gmail.com>
2024-02-04 16:13:16 -0500

    fix: fix banner

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,41 +1,42 @@
-import React from 'react'
-import './Header.css';
-import header from '../assets/h1.jpg';
-import BoberwArt from '../assets/BoberwArt.jpg';
+import React from "react";
+import "./Header.css";
+import header from "../assets/h1.jpg";
+import BoberwArt from "../assets/BoberwArt.jpg";
 import Image from "next/image";
-import Button from './Button'
-import Link from 'next/link';
-
+import Button from "./Button";
+import Link from "next/link";
 
 function Header() {
   return (
-    
-    <div className='Header' >
-    <Image
-    className="header-bg"
-    src= {header}
-    alt=""
-    />
-      <div className='Header_components'>
+    <div className="Header">
+      <Image
+        className="w-screen opacity-60 hidden md:block"
+        src={header}
+        alt=""
+      />
 
-        <article className='Header_side'>
-          <h2>Ziggy Bober Design</h2>
-          <h1>Professional Interior Design Company in New-York </h1>
-          <p > "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.  </p>
+      <article className="Header_side">
+        <h2>Ziggy Bober Design</h2>
+        <h1>Professional Interior Design Company in New-York </h1>
+        <p>
+          {" "}
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.{" "}
+        </p>
 
-          <Button
-            styles={'custom__button'}
-            type={'button'}
-            title={'Open modal'}
-            disabled={false}
-            >
-              <Link href="/about_page">  Learn About Us</Link>
-            </Button>
-        </article>
-      
-      </div>
+        <Button
+          styles={"custom__button"}
+          type={"button"}
+          title={"Open modal"}
+          disabled={false}
+        >
+          <Link href="/about_page"> Learn About Us</Link>
+        </Button>
+      </article>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
