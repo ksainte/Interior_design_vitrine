0707c87f284dbaa295b7d68765f7041bf96ebded
ks <ksainte19@gmail.com>
2024-02-04 01:21:28 +0100

    14th commit

--- src/app/about_page/About_us.tsx
+++ src/app/about_page/About_us.tsx
@@ -13,7 +13,7 @@ function About_us() {
                 <article className='About_leftside1'>
                 <h1>Past Works And What's Coming...</h1>
                 
-                <p >  <Image src={BoberwArt} alt="header_img" />
+                <p> <Image src={BoberwArt} alt="header_img" />
 
                  <p >"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "Lorem ipsum dolor sit amet. </p>         
                 
