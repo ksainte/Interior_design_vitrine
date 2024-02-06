72de5fdf036ce6a0bfa996429f042a55bd688164
ks <ksainte19@gmail.com>
2024-02-07 00:54:06 +0100

    23th commit

--- src/app/components/Our_Services.tsx
+++ src/app/components/Our_Services.tsx
@@ -78,7 +78,7 @@ function Our_Services() {
          
           <p > "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.  </p>
           <Button
-            styles={'custom__button2'}
+            styles={'custom__button1'}
             type={'button'}
             title={'Open Art'}
             disabled={false}
