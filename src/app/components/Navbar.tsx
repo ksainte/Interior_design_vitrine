8c915c25f8ce7398c1315d71969080d1f2b38516
ks <ksainte19@gmail.com>
2024-02-08 01:47:44 +0100

    46th commit

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -16,9 +16,9 @@ export default function Navbar() {
     <div className="navbar w-screen" id="navbar">
       <div className="navbar_comp">
           <div className="navbar_left">
-          <a href="/">
-            <Image className="test"src={logo} alt="LinkedIn" />
-            </a>
+          <Link href="/">
+            <Image className="testing"src={logo} alt="LinkedIn" />
+            </Link>
           </div>
 
         <div className="navbar_right">
